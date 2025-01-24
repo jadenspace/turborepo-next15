36588aa9df226f3bbf1d36c61bfb26eab865605f
KimYeonho <monster901@gentlemonster.com>
2025-01-24 19:06:30 +0900

    chore: tailwindcss grid 설정

--- packages/config/tailwindcss-config/index.js
+++ packages/config/tailwindcss-config/index.js
@@ -1,4 +1,5 @@
 import lineClamp from "@tailwindcss/line-clamp";
+import gridAreas from "@savvywombat/tailwindcss-grid-areas";
 
 export default {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./src/**/*.{js,ts,jsx,tsx,mdx}"],
@@ -6,6 +7,6 @@ export default {
     extend: {
       // 여기에 테마 확장 설정 추가
     },
-    plugins: [lineClamp],
+    plugins: [lineClamp, gridAreas],
   },
 };
